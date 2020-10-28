171a2a21ddf13a9bb984e95a21a381154f8d10c8
Ana GOMEZ-MAYEN <31400695+anagomezmayen@users.noreply.github.com>
2020-10-28 00:57:45 -0700

    arrays

---
+++ CIP/ArraysProblems.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIP
+{
+    class ArraysProblems
+    {
+        /// <summary>
+        /// Write a program to print all the LEADERS in the array. An element is leader if it is greater than all the elements to 
+        /// its right side. And the rightmost element is always a leader. For example int the array {16, 17, 4, 3, 5, 2}, 
+        /// leaders are 17, 5 and 2.
+        /// </summary>
+        /// <param name="a"></param>
+        static public void findAllLeaders(int[] a)
+        {
+            if (a == null)
+                return;
+            
+            int leader = a[a.Length-1];
+            Console.Write("Leaders: {0} ", leader);
+            if (a.Length == 1)
+                return;
+
+            for (int j=a.Length-2; j>=0;j--)
+            {
+                if (a[j] > leader)
+                {
+                    leader = a[j];
+                    Console.Write(leader + "  ");
+                }
+            }
+
+        }
+
+        static public void Main(string[] args){
+            int[] numbers = new int[3];
+
+            findAllLeaders(numbers);
+        }
+
+    }
+}
--- CIP/Problem1.cs
+++ CIP/Problem1.cs
@@ -40,17 +40,126 @@ namespace CIP
                Console.WriteLine("-1");
         }
 
-            static public void Main(string[] args)
+        /// <summary>
+        /// 
+        /// Write a program to sort an array of 0's,1's and 2's in ascending order.
+        /// </summary>
+        /// <param name="a"></param>
+        static public void SortZerosAndOnes(int[] a)
         {
-            int[] a = { 1, 4, 0, 0, 3, 10, 5 };
-            int[] b = { 1, 2, 3, 4, 5 };
-            int[] c = { 1, 4, 20, 3, 10, 5 };
-
-            SortedSubarray(a, -1);
-            SortedSubarray(b, 5);
-            SortedSubarray(c, 33);
-            
+            foreach (int x in a)
+            {
+                Console.Write(x + "  ");
             }
+            Console.WriteLine();
+
+            int i=0, j=0, k=a.Length-1;
+
+            for (; k >= 0; k--)
+            {
+                if (a[k] != 2) 
+                    break;
+            }
+
+            for (j = 0; j <= k; j++)
+            {
+                if (a[j] == 2)
+                {
+                    a[j] = a[k];
+                    a[k] = 2;
+                    k--;
+                    j--;
+                }
+                else
+                {
+                    if (a[j] == 0)
+                    {
+                        a[j] = a[i];
+                        a[i] = 0;
+                        i++;
+                    }
+                }
+            }
+
+            foreach(int x in a)
+            {
+                Console.Write(x + "  ");
+            }
+        }
+
+
+        //Question 9
+        static public void printSpiralArray(int[,] arr)
+        {
+            int x = arr.GetLength(1);// obtaing length of x
+            int y = arr.GetLength(0); //length of y
+
+            int j = 0, i = 0, layer = 1;
+            while (layer <= x / 2 && layer <= y / 2)
+            {
+                while (i < (x - layer))//->
+                {
+                    Console.Write(arr[j, i] + " ");
+                    i++;
+                }
+                while (j < (y - layer))
+                {
+                    Console.Write(arr[j, i] + " ");
+                    j++;
+                }
+                while (i > (layer - 1))//<-
+                {
+                    Console.Write(arr[j, i] + " ");
+                    i--;
+                }
+                while (j > (layer - 1))
+                {
+                    Console.Write(arr[j, i] + " ");
+                    j--;
+                }
+                j = i = layer;
+                layer++;
+            }
+            layer--;
+            if (y % 2 == 1 && x % 2 == 1)
+            {
+                Console.Write(arr[i, j]);
+            }
+            if (y % 2 == 0 && x % 2 == 1)
+            {
+                while (j < y - layer)
+                {
+                    Console.Write(arr[j, i] + " ");
+                    j++;
+                }
+            }
+            if (x % 2 == 0 && y % 2 == 1)
+            {
+                while (i < x - layer)
+                {
+                    Console.Write(arr[j, i] + " ");
+                    i++;
+                }
+            }
+        }
+
+        //0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2
+
+        //static public void Main(string[] args)
+        //{
+        //    int[] z = { 0, 1, 2, 2, 2, 1, 1, 0, 0, 1, 1, 2, 0 };
+
+        //    SortZerosAndOnes(z);
+
+        //    //int[] a = { 1, 4, 0, 0, 3, 10, 5 };
+        //    //int[] b = { 1, 2, 3, 4, 5 };
+        //    //int[] c = { 1, 4, 20, 3, 10, 5 };
+
+        //    //SortedSubarray(a, -1);
+        //    //SortedSubarray(b, 5);
+        //    //SortedSubarray(c, 33);
+            
+        //    }
 
     }
 }
