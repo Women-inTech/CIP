822b5a8bce1c2a1079caeb5b54a4d280ff27bac7
Ana GOMEZ-MAYEN <31400695+anagomezmayen@users.noreply.github.com>
2020-11-03 22:42:19 -0800

    arrays

---
+++ CIP/Arrays.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CIP
+{
+    class Arrays
+    {
+        static public bool IsMatch(string s1, string s2)
+        {
+            if (s1.Length != s2.Length)
+                return false;
+            for(int i = 0; i < s1.Length; i++)
+            {
+                if (s2[i] == '*')
+                {
+                    continue;
+                }
+                if (s2[i] == '#')
+                {
+                    if (!Char.IsNumber(s1[i]))
+                        return false;
+                    continue;
+                }
+
+                if (s1[i] != s2[i])
+                    return false;
+            }
+            return true;
+        }
+
+        static public int FindKth(int[] a, int k)
+        {
+            int[] sa = MergeSort(a);
+            return (sa[k-1]);
+        }
+
+        static public int[] MergeSort(int[] array)
+        {
+            int n = array.Length;
+
+            if (n == 1)
+                return array;
+
+            int[] a = new int[n / 2];
+            int[] b = new int[(n / 2) + (n % 2)];
+
+            int i = 0, j=0;
+            while (i < a.Length)
+            {
+                a[i] = array[i];
+                i++;
+            }
+            while (i < array.Length)
+            {
+                b[j]= array[i];
+                j++;
+                i++;
+            }
+
+            return Merge(MergeSort(a), MergeSort(b));
+
+
+
+            
+
+
+
+
+        }
+
+        static public int[] Merge(int[] a, int[] b)
+        {
+            int[] result = new int[a.Length + b.Length];
+
+            int i = 0, j = 0, k=0; //i-a, j-b, k-r
+
+            while(i<a.Length && j < b.Length)
+            {
+                if (a[i] < b[j])
+                {
+                    result[k]=a[i];
+                    i++;
+                }
+                else
+                {
+                    result[k] = b[j];
+                    j++;
+                }
+                k++;
+            }
+            if (i == a.Length && j < b.Length)
+            {
+                while (k < result.Length)
+                {
+                    result[k] = b[j];
+                    k++;
+                    j++;
+                }
+            }
+            if (j == b.Length && i < a.Length)
+            {
+                while (k < result.Length)
+                {
+                    result[k] = a[i];
+                    k++;
+                    i++;
+                }
+            }
+  
+            return result;
+
+        }
+
+
+        static public void Main(string[] args)
+        {
+            int[] a1 = {3, 2, 5, 7, 8, 4};
+            int[] b1 = { 0, 1, 6, 10 };
+
+            int[] x = MergeSort(a1);
+
+            foreach(int c in x)
+            {
+                Console.Write(c + " , ");
+            }
+
+
+            Console.WriteLine("\n"+ FindKth(a1, 3));
+           
+            //Console.WriteLine(IsMatch("ab31z", "ab31z"));
+            //Console.WriteLine(IsMatch("ab31z", "ab#1"));
+
+            //Console.WriteLine(IsMatch("ab31z", "ab*1#"));
+
+
+        }
+    }
+}
--- CIP/ArraysProblems.cs
+++ CIP/ArraysProblems.cs
@@ -33,11 +33,108 @@ namespace CIP
 
         }
 
-        static public void Main(string[] args){
-            int[] numbers = new int[3];
+        static long repeatedString(string s, long n)
+        {
+            Console.WriteLine("s.lenght: "+s.Length+"   "+"n: " + n);
 
-            findAllLeaders(numbers);
+            if (s.Length >= n)
+            {//only count the a
+                return countA(s.Substring(0, (int)n));
+            }
+            long numAonS = countA(s), sum = 0;
+            if (numAonS == 0)
+                return 0;
+
+            if (s.Length == 1)
+                return n*numAonS;
+
+            Console.WriteLine("----->"+numAonS);
+            long diff = n - s.Length;
+            Console.WriteLine("Diff:   " + diff);
+            sum = numAonS + ((diff / s.Length) * numAonS) + countA(s.Substring(0, (int)(diff % s.Length)));
+            return sum;
         }
+
+        static private long countA(string s)
+        {
+            long numAs = 0;
+            foreach (char c in s)
+            {
+                if (c == 'a')
+                    numAs++;
+            }
+            return numAs;
+        }
+
+        static int birthday(List<int> s, int d, int m)
+        {
+            int[] chocolateBar = s.ToArray();
+            if(chocolateBar.Length==1 && m == 1)
+            {
+                if(chocolateBar[0] == d)
+                    return 1;
+                return -1;
+            }
+            int total = 0, result=0;
+            int length_tmp = 1;
+            for(int i = 0; i < chocolateBar.Length-1; i++)
+            {
+                total = chocolateBar[i];
+                length_tmp = 1;
+                for(int j=i+1;j <=s.Count-1; j++)
+                {
+                    total += chocolateBar[j];
+                    length_tmp++;
+                    if (length_tmp >= m)
+                    {
+                        if (total == d)
+                            result++;
+                        break;
+                    }
+                }
+
+            }
+            return result;
+        }
+
+        static int workbook(int n, int k, int[] arr)
+        {
+            int numProblem = 1;
+            int spetialProblems = 0;
+            int temp, x = 0, page = 1;
+            for(int i = 0; i < n; i++)
+            {
+                temp = arr[i] / k;
+
+                for (x = 1; x <= temp; x++) { //per page
+                    numProblem = ((x - 1) * k) + 1; //first problem on page
+                    if (page >= numProblem && page <= numProblem + k - 1)
+                    {
+                        spetialProblems++;
+
+                    }
+                    page++;
+                }
+                if(arr[i]%k > 0)
+                {
+                    numProblem = (temp * k) + 1;
+                    if (page >= numProblem && page <= numProblem + (arr[i] % k - 1))
+                        spetialProblems++;
+                    page++;
+                }
+                
+            }
+            return spetialProblems;
+        }
+
+
+        //static public void Main(string[] args){
+        //    int[] numbers = { 4, 2, 6, 1, 10 };
+
+        //    Console.WriteLine(workbook(5,3,numbers)); //4
+        
+        //}
+
 
     }
 }
